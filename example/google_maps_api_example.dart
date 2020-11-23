6f5d81e453473f14a1bbe790bc961a3d1166a1b4
Fordyta Abubakar <dhytodev@gmail.com>
2020-11-23 18:01:00 +0800

    initial projects

---
+++ example/google_maps_api_example.dart
@@ -0,0 +1,6 @@
+import 'package:google_maps_api/google_maps_api.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/google_maps_api.dart
@@ -0,0 +1,6 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library google_maps_api;
+
+export 'src/google_maps_api_base.dart';
