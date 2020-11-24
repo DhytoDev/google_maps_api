30b3b8cee70254b53278312440fcc33e6b3f466c
Fordyta Abubakar <dhytodev@gmail.com>
2020-11-24 09:38:23 +0800

    add example

---
+++ example/bin/main.dart
@@ -0,0 +1,23 @@
+import 'package:google_maps_api/places.dart';
+import 'package:logging/logging.dart';
+
+void main() async {
+  _setupLogging();
+
+  final places = await Places.findNearbyPlaces(
+    input: 'Helper',
+    apiKey: 'AIzaSyDmHjXhdz0DtdRm-WPVfFYP91KQcqovqyA',
+    radius: 50,
+    location: Location(-5.1921861, 119.4807209),
+  );
+
+  print(places.status);
+  print(places.predictions.map((e) => e.toJson()).toList());
+}
+
+void _setupLogging() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((rec) {
+    print(rec.message);
+  });
+}
