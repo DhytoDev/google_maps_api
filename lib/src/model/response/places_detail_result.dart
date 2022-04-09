00a87420228371e77df2a6362d91f3f4787bd3ac
Fordyta Abubakar <dhytodev@gmail.com>
2022-04-09 09:14:17 +0800

    feat: update PlaceDetailsResult model

--- lib/src/model/response/places_detail_result.dart
+++ lib/src/model/response/places_detail_result.dart
@@ -1,48 +1,50 @@
-import 'package:google_maps_api/places.dart';
-import 'package:google_maps_api/src/model/address_component.dart';
-import 'package:google_maps_api/src/model/response_status.dart';
+import 'package:google_maps_api/google_maps_api.dart';
 import 'package:json_annotation/json_annotation.dart';
+
+import '../address_component.dart';
+import '../response_status.dart';
+import 'base_result.dart';
 
 part 'places_detail_result.g.dart';
 
-@JsonSerializable()
-class PlaceDetailResult extends ResponseStatus {
-  final Result? result;
+@JsonSerializable(createToJson: false)
+class PlaceDetailsResult extends ResponseStatus {
+  final PlaceDetailsData? result;
 
-  PlaceDetailResult(
+  PlaceDetailsResult(
     String? status,
     String? errorMessage,
     this.result,
   ) : super(status, errorMessage);
 
-  factory PlaceDetailResult.fromJson(Map<String, dynamic> json) =>
-      _$PlaceDetailResultFromJson(json);
+  factory PlaceDetailsResult.fromJson(Map<String, dynamic> json) =>
+      _$PlaceDetailsResultFromJson(json);
 
-  Map<String, dynamic> toJson() => _$PlaceDetailResultToJson(this);
+  // Map<String, dynamic> toJson() => _$PlaceDetailResultToJson(this);
+
+  @override
+  List<Object?> get props => [result];
 }
 
-@JsonSerializable()
-class Result {
-  @JsonKey(name: 'address_components')
-  List<AddressComponent>? addressComponents;
-  final Geometry? geometry;
-  @JsonKey(name: 'business_status')
-  final String? businessStatus;
-  @JsonKey(name: 'formatted_address')
-  final String? formattedAddress;
+@JsonSerializable(createToJson: false)
+class PlaceDetailsData extends BaseResult {
   final String? name;
   @JsonKey(name: 'international_phone_number')
   final String? phoneNumber;
 
-  Result(
-    this.geometry,
-    this.businessStatus,
-    this.formattedAddress,
+  PlaceDetailsData(
+    List<AddressComponent?>? addressComponents,
+    String? formattedAddress,
+    Geometry? geometry,
+    String? placeId,
+    List<String>? types,
     this.name,
     this.phoneNumber,
-  );
+  ) : super(addressComponents, formattedAddress, geometry, placeId, types);
 
-  factory Result.fromJson(Map<String, dynamic> json) => _$ResultFromJson(json);
+  factory PlaceDetailsData.fromJson(Map<String, dynamic> json) =>
+      _$PlaceDetailsDataFromJson(json);
 
-  Map<String, dynamic> toJson() => _$ResultToJson(this);
+  @override
+  List<Object?> get props => super.props;
 }
--- lib/src/model/response/places_detail_result.g.dart
+++ lib/src/model/response/places_detail_result.g.dart
@@ -6,42 +6,28 @@ part of 'places_detail_result.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-PlaceDetailResult _$PlaceDetailResultFromJson(Map<String, dynamic> json) {
-  return PlaceDetailResult(
-    json['status'] as String?,
-    json['error_message'] as String?,
-    json['result'] == null
-        ? null
-        : Result.fromJson(json['result'] as Map<String, dynamic>),
-  );
-}
+PlaceDetailsResult _$PlaceDetailsResultFromJson(Map<String, dynamic> json) =>
+    PlaceDetailsResult(
+      json['status'] as String?,
+      json['error_message'] as String?,
+      json['result'] == null
+          ? null
+          : PlaceDetailsData.fromJson(json['result'] as Map<String, dynamic>),
+    );
 
-Map<String, dynamic> _$PlaceDetailResultToJson(PlaceDetailResult instance) =>
-    <String, dynamic>{
-      'status': instance.status,
-      'error_message': instance.errorMessage,
-      'result': instance.result,
-    };
-
-Result _$ResultFromJson(Map<String, dynamic> json) {
-  return Result(
-    json['geometry'] == null
-        ? null
-        : Geometry.fromJson(json['geometry'] as Map<String, dynamic>),
-    json['business_status'] as String?,
-    json['formatted_address'] as String?,
-    json['name'] as String?,
-    json['international_phone_number'] as String?,
-  )..addressComponents = (json['address_components'] as List<dynamic>?)
-      ?.map((e) => AddressComponent.fromJson(e as Map<String, dynamic>))
-      .toList();
-}
-
-Map<String, dynamic> _$ResultToJson(Result instance) => <String, dynamic>{
-      'address_components': instance.addressComponents,
-      'geometry': instance.geometry,
-      'business_status': instance.businessStatus,
-      'formatted_address': instance.formattedAddress,
-      'name': instance.name,
-      'international_phone_number': instance.phoneNumber,
-    };
+PlaceDetailsData _$PlaceDetailsDataFromJson(Map<String, dynamic> json) =>
+    PlaceDetailsData(
+      (json['address_components'] as List<dynamic>?)
+          ?.map((e) => e == null
+              ? null
+              : AddressComponent.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      json['formatted_address'] as String?,
+      json['geometry'] == null
+          ? null
+          : Geometry.fromJson(json['geometry'] as Map<String, dynamic>),
+      json['place_id'] as String?,
+      (json['types'] as List<dynamic>?)?.map((e) => e as String).toList(),
+      json['name'] as String?,
+      json['international_phone_number'] as String?,
+    );
