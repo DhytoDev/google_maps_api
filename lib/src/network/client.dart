809fc0f95bb20708722e8b9de75cd93f3df972e6
Fordyta Abubakar <dhytodev@gmail.com>
2022-04-09 09:17:43 +0800

    feat: remove services from client

--- lib/src/network/client.dart
+++ lib/src/network/client.dart
@@ -1,12 +1,8 @@
 import 'package:chopper/chopper.dart';
-import 'package:google_maps_api/src/network/services/place_service.dart';
 
 ChopperClient getClient() {
   return ChopperClient(
     baseUrl: 'https://maps.googleapis.com/maps/api',
-    services: [
-      PlaceService.create(),
-    ],
     converter: JsonConverter(),
     interceptors: [
       HttpLoggingInterceptor(),
