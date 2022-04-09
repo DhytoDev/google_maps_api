37ad61a8d8ebf5d1ec8fc451bae93cdbafde7265
Fordyta Abubakar <dhytodev@gmail.com>
2022-04-09 09:18:36 +0800

    feat: manage the lib exports

---
+++ lib/google_maps_api.dart
@@ -0,0 +1,9 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library google_maps_api;
+
+export 'src/domain/geocoding.dart';
+export 'src/domain/google_maps_api.dart';
+export 'src/domain/places.dart';
+export 'src/model/model.dart';
--- lib/places.dart
+++
@@ -1,8 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library google_maps_api.places;
-
-export 'src/domain/places.dart';
-export 'src/domain/google_maps_api.dart';
-export 'src/model/geometry.dart';
